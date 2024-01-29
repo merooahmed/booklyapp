f1d1b020653abc89f05e9e3341a9de1beab46667
Marwa Ahmed <126021568+merooahmed@users.noreply.github.com>
2024-01-29 23:20:05 +0200

    add getit

--- lib/Features/Home/data/repos/home_repo.dart
+++ lib/Features/Home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:booklyapp/Features/Home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/core/erros/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchNewestBooks();
-  fetchFeauteredBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeauteredBooks();
 }
